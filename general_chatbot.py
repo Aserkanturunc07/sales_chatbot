0e658923a50290c401bf092e824f6063661a0589
Ahmet Serkan Turunc <91109649+Aserkanturunc07@users.noreply.github.com>
2023-11-04 19:42:47 -0400

    Update general_chatbot.py

--- general_chatbot.py
+++ general_chatbot.py
@@ -11,7 +11,7 @@ from langchain.schema import (
 )
 
 # Placing your actual OpenAI API key here
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "your_api_key"
 
 # Initializing the chat model with the OpenAI API key and preferred model settings
 chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.2, openai_api_key=OPENAI_API_KEY)
