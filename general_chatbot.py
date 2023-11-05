90c786ce2f720ddf7a700f2277709261a268541f
Ahmet Serkan Turunc <91109649+Aserkanturunc07@users.noreply.github.com>
2023-11-04 20:34:07 -0400

    Add files via upload

---
+++ general_chatbot.py
@@ -0,0 +1,40 @@
+"""
+This module implements a basic chatbot using OpenAI's language model.
+The chatbot interacts with the user in a conversational manner and can handle a variety of topics.
+"""
+import pandas as pd
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+
+# Placing your actual OpenAI API key here
+OPENAI_API_KEY = "ssk-pUckn1XhUnczjNbgcECFT3BlbkFJIr3GDALLL6RcKQg7dsEA"
+
+# Initializing the chat model with the OpenAI API key and preferred model settings
+chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.2, openai_api_key=OPENAI_API_KEY)
+
+def chatbot():
+    """
+    Initiates and manages a conversation with the user. The chatbot continues interacting
+    until the user types 'exit'.
+    """
+    print("Hello! I am a Chatbot. Type 'exit' to end the conversation.")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == "exit":
+            print("Chatbot: Goodbye!")
+            break
+
+        # Creating the message history context for the AI, including the latest user message
+        messages = [HumanMessage(content=user_input)]
+
+        # Generating the AI's response considering the current conversation context
+        chatbot_response = chat(messages)
+
+        print(f"Chatbot: {chatbot_response.content}")
+
+if __name__ == "__main__":
+    chatbot()
